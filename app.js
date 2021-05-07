93e8044bd1efac316f9288c79203a9d34a46c3bb
a3darekar <darekar.amey@gmail.com>
2021-05-07 16:48:38 +0530

    dotenv cofig step 2

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
 const axios = require('axios')
-const interval = 3; 
+const interval = 3;
 const userAgent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'
+const dotenv = require('dotenv');
+const dateformat = require('dateformat');
+dotenv.config();
+let args = process.env;
 
 
-function ping() {
+function ping(parameters) {
 	axios.get(
-			"https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=411011&date=08-05-2021", 
+			"https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByPin?pincode=" + parameters.pincode + "&date=" + parameters.date, 
 			{headers: {'User-Agent': userAgent}}
 	).then(
 		(result) => {
 			console.log(result.data);
+		}, (error) => {
+			console.log(error);
 	});
 }
 
 function getParameters() {
-	if (argv.help){
+	if (args.help){
 		console.error("Visit https://github.com/a3darekar/cowin-notifier#readme for documentation.");
 		return;
-	} else if(argv.ifttt && typeof argv.ifttt !== 'string') {
+	} else if(args.ifttt && typeof args.ifttt !== 'string') {
 		console.error('Please provide a valid IFTTT Webhook Key by appending --ifttt=<IFTTT-WEBHOOK-NAME> to recieve mobile notifications on IFTTT \nRefer documentation for more details');		
+		console.error("Visit https://github.com/a3darekar/cowin-notifier#readme for documentation.");
 		return;
-	} else if(argv.hook && !argv.key || !argv.hook && argv.key) {
+	} else if(args.ifttt && !args.key || !args.ifttt && args.key) {
 		console.error('Please provide valid IFTTT Webhook Name key and a valid IFTTT Webhook Key');
+		console.error("Visit https://github.com/a3darekar/cowin-notifier#readme for documentation.");
 		return;
-	} else if(!argv.age) {
+	} else if(!args.age) {
 		console.error('Please provide valid minimum age key to recieve proper alerts');
+		console.error("Visit https://github.com/a3darekar/cowin-notifier#readme for documentation.");
 		return;
-	}
-	} else if(!argv.pin) {
-			console.error('Please provide required PIN by appending --pin=<PIN-CODE> \nRefer documentation for more details');
+	} else if(!args.pincode) {
+		console.error('Please provide required PIN by appending --pin=<PIN-CODE> \nRefer documentation for more details');
+		console.error("Visit https://github.com/a3darekar/cowin-notifier#readme for documentation.");
 		return;
 	} else {
-		let today = new Date();
+		let date = new Date()
+		date.setDate(date.getDate() + 1);
 		const parameters = {
-			key: argv.key,
-			ifttt: argv.ifttt,
-			age: argv.age,
-			pinCode: argv.pin,
-			date: format(today.getDate() + 1, 'dd-MM-yyyy')
+			key: args.key,
+			ifttt: args.ifttt,
+			age: args.age,
+			pincode: args.pincode,
+			date: dateformat(date, 'dd-mm-yyyy')
 		}
 		console.log(parameters);
+		ping(parameters);
 	}
 }
 
 function run(){
 	getParameters();
 }
-ping();
+
+run();
